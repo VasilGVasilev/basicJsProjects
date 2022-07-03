ad0f631fb0c04edf703202cdb1bfa1285106f1be
VasilGVasilev <vaskomeil@gmail.com>
2022-07-03 20:46:57 +0300

    hex colour update

--- changeBackgorundColour/script.js
+++ changeBackgorundColour/script.js
@@ -1,11 +1,17 @@
+const hexNums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+
 const colourBtn = document.querySelector('.colourBtn');
 const body = document.querySelector('body');
-
-const colour = ['red', 'yellow', 'green', '#3b5998'];
+const hex = document.querySelector('.hex')
 
 colourBtn.addEventListener('click', changeColour);
 
 function changeColour(){
-    let random = Math.floor(Math.random()*colour.length);
-    body.style.backgroundColor = colour[random];
+    let hexColour = '#';
+    for (let i = 0; i < 6; i++){
+        let random = Math.floor(Math.random()*hexNums.length);
+        hexColour+=hexNums[random];
+    }
+    body.style.backgroundColor = hexColour;
+    hex.innerHTML = hexColour;
 }
